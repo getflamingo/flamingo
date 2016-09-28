f7092ce3f2c01cdfa93428f2f17227884a1e58e7
Manu <e.vodor@gmail.com>
2016-09-29 00:21:30 +0200

    [TASK] Added basic box conf to generate standalone phar

---
+++ bin/main.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$script = new \Flamingo\Core\Script(
+    file_get_contents(getcwd() . '/flamingo.yml')
+);
+
+$script->run(!empty($argv[1]) ? $argv[1] : 'default');
