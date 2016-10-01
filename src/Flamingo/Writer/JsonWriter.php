21f85309a071f4219b8d588f265d48f17f75ab09
Manu <e.vodor@gmail.com>
2016-10-01 14:50:20 +0200

    [BUGFIX] JsonWriter: Table should be casted to array

--- src/Flamingo/Writer/JsonWriter.php
+++ src/Flamingo/Writer/JsonWriter.php
@@ -19,7 +19,7 @@ abstract class JsonWriter implements Writer
         $filename = !empty($options['file']) ? $options['file'] : '';
 
         // Encode data
-        $json = json_encode($table);
+        $json = json_encode((array)$table);
 
         // Write output
         file_put_contents($filename, $json);
