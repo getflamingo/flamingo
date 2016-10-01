c3f042e27e68831fef7a8515684db3b73d99b60d
Manu <e.vodor@gmail.com>
2016-10-01 19:48:50 +0200

    [FEATURE] Add usage output in cli mode

--- bin/main.php
+++ bin/main.php
@@ -2,9 +2,26 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$script = new \Flamingo\Core\Flamingo(
-    file_get_contents(__DIR__ . '/default.yml'),
+$flamingo = new \Flamingo\Core\Flamingo(
+    file_get_contents(__DIR__ . '/default.yml')
+);
+
+$name = $GLOBALS['FLAMINGO']['CONF']['App']['Name'];
+$version = $GLOBALS['FLAMINGO']['CONF']['App']['Version'];
+$usage = $GLOBALS['FLAMINGO']['CONF']['App']['Usage'];
+
+$flamingo->addConfiguration(
     file_get_contents(getcwd() . '/flamingo.yml')
 );
 
-$script->run(!empty($argv[1]) ? $argv[1] : 'default');
+if (in_array('-h', $argv) || in_array('--help', $argv)) {
+    echo $usage;
+    exit;
+}
+
+if (in_array('-v', $argv) || in_array('--version', $argv)) {
+    echo $name . ' ' . $version;
+    exit;
+}
+
+$flamingo->run(!empty($argv[1]) ? $argv[1] : 'default');
