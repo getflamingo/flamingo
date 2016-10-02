d3a8c26381c98d6b81528e70cc81d70f497ed39c
Manu <e.vodor@gmail.com>
2016-10-02 18:44:10 +0200

    [TASK] Add str_replace modifier

---
+++ src/Flamingo/Process/Modifier/StrReplaceModifier.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Flamingo\Process\Modifier;
+
+use Analog\Analog;
+
+/**
+ * Class StrReplaceModifier
+ * @package Flamingo\Process\Modifier
+ */
+class StrReplaceModifier implements ModifierInterface
+{
+    /**
+     * @param string $value
+     * @param mixed $options
+     * @param array $record
+     */
+    public function process(&$value, $options, $record)
+    {
+        if (count($options) !== 2) {
+            Analog::error('StrReplace: Must have 2 arguments');
+            return;
+        }
+
+        $value = str_replace($options[0], $options[1], $value);
+    }
+}
