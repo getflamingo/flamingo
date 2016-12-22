7bff44340dc0a84f829010547bfef2fc82b159ea
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2016-12-22 15:04:18 +0100

    [FEATURE] Add mapping process to rename column names (+ alias)

---
+++ src/Flamingo/Process/MappingProcess.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Flamingo\Process;
+
+use Analog\Analog;
+use Flamingo\Core\Process;
+use Flamingo\Core\Task;
+
+/**
+ * Class MappingProcess
+ * @package Flamingo\Process
+ */
+class MappingProcess extends Process
+{
+    /**
+     * MappingProcess constructor.
+     * Clean up the configuration array before using it
+     *
+     * @param array $configuration
+     */
+    public function __construct($configuration)
+    {
+        // No modifier specified
+        if (!is_array($configuration)) {
+            Analog::warning('Mapping array is empty or null');
+            return;
+        }
+
+        foreach ($configuration as $key => $newKey) {
+
+            // Support sub arrays syntax
+            if (is_array($newKey)) {
+                $key = current(array_keys($newKey));
+                $newKey = current(array_values($newKey));
+            }
+
+            // Add non null mapping values to object conf
+            if (!empty($newKey)) {
+                $this->configuration[$key] = $newKey;
+            }
+        }
+    }
+
+    /**
+     * Remap some keys
+     *
+     * @param array $data
+     * @return int
+     */
+    public function execute(&$data)
+    {
+        foreach ($data as &$table) {
+            foreach ($table as &$record) {
+
+                // Backup current record
+                $baseRecord = $record;
+
+                // Apply mapping
+                foreach ($this->configuration as $key => $newKey) {
+                    if (array_key_exists($key, $baseRecord)) {
+                        $record[$newKey] = $baseRecord[$key];
+                        unset($record[$key]);
+                    }
+                }
+            }
+        }
+
+        return Task::OK;
+    }
+}
