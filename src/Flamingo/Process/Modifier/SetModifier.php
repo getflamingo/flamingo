6415cc5c42325a5b38e84297d8510a85f76630eb
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2016-12-22 17:26:04 +0100

    [FEATURE] Modify: Add basic "set" modifier

---
+++ src/Flamingo/Process/Modifier/SetModifier.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Flamingo\Process\Modifier;
+
+/**
+ * Class SetModifier
+ * @package Flamingo\Process\Modifier
+ */
+class SetModifier implements ModifierInterface
+{
+    /**
+     * @param mixed $value
+     * @param mixed $newValue
+     * @param array $record
+     */
+    public function process(&$value, $newValue, $record)
+    {
+        $value = $newValue;
+    }
+}
