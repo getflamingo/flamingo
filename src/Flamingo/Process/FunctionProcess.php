eb59901411ece0cb8b4b92b61492bfcae639a44e
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2017-01-12 14:38:11 +0100

    [FEATURE] Function: Add user function process!

---
+++ src/Flamingo/Process/FunctionProcess.php
@@ -0,0 +1,42 @@
+<?php
+namespace Flamingo\Process;
+
+/**
+ * Class FunctionProcess
+ * @package Flamingo\Process
+ */
+class FunctionProcess extends AbstractProcess
+{
+    /**
+     * FunctionProcess constructor.
+     * @param array $configuration
+     */
+    public function __construct($configuration)
+    {
+        if (is_string($configuration)) {
+            $configuration = [$configuration];
+        }
+
+        parent::__construct($configuration);
+    }
+
+    /**
+     * Call needed user functions
+     *
+     * @param array $data
+     * @return int
+     */
+    public function execute(&$data)
+    {
+        foreach ($this->configuration as $callable) {
+
+            // Replace arrow to static call
+            $callable = str_replace('->', '::', $callable);
+
+            // Call function if it exists
+            if (strpos($callable, '::') || function_exists($callable)) {
+                call_user_func($callable, $data);
+            }
+        }
+    }
+}
