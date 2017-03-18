28a1665a2842e3433c3e8260d2ed07464e042350
Manu <e.vodor@gmail.com>
2017-03-18 20:22:50 +0100

    [FEATURE] Rework query process using Yalinqo implementation

--- src/Flamingo/Process/QueryProcess.php
+++ src/Flamingo/Process/QueryProcess.php
@@ -19,104 +19,61 @@ class QueryProcess extends AbstractProcess
      */
     public function execute(&$data)
     {
-        // Execute query on first source
-        $source = $this->query($data, $this->configuration);
+        $newData = [];
 
-        // Create result table
-        $table = new Table;
-        $table->copy($source->toArrayDeep());
+        foreach ($this->configuration as $configuration) {
 
-        // Insert table into data
-        $data = [$table];
+            // Execute 1st level query
+            $request = $this->executeQuery($data, $configuration);
+
+            // Insert request data into a new table
+            $table = new Table;
+            $table->copy($request);
+
+            // Push to data
+            $newData[] = $table;
+        }
+
+        $data = $newData;
     }
 
     /**
      * Execute a query on data array
-     * This has to have its own method to handle recursion
      *
      * @param array $data
      * @param array $configuration
      * @return \YaLinqo\Enumerable
      */
-    protected function query(&$data, $configuration)
+    protected function executeQuery(&$data, $configuration)
     {
-        $defaultOptions = [
-            'source' => null,
-            'orderBy' => null,
-            'join' => null,
-            'where' => null,
-        ];
+        // Determine source identifier
+        $identifier = $configuration['from'];
+        unset($configuration['from']);
 
-        $options = array_replace($defaultOptions, $configuration);
+        // Fetch source from original
+        $source = iterator_to_array($data[$identifier]);
+        $source = \from($source);
 
-        if (!is_numeric($index = $options['source'])) {
-            Analog::error('Source is undefined');
-            return null;
-        }
+        // Use query options on source
+        foreach ($configuration as $option => $parameters) {
 
-        if (!array_key_exists($index, $data)) {
-            Analog::error(sprintf('No source found at index "%s"', $index));
-            return null;
-        }
-
-        // Create query from the selected source
-        $request = \from($data[$index]);
-
-        // Order by a field name
-        if (!is_null($orderBy = $options['orderBy'])) {
-            $request = $request->orderBy(function ($record) use ($orderBy) {
-                return $record[$orderBy];
-            });
-        }
-
-        // Join with another data source
-        if (is_array($joinConstraints = $options['join'])) {
-
-            $joinValues = [];
-
-            foreach ($joinConstraints as $k => $join) {
-
-                $defaultJoin = [
-                    'source' => null,
-                    'local' => null,
-                    'foreign' => null,
-                ];
-
-                $join = array_replace($defaultJoin, $join);
-
-                // Join current data with a selected source
-                $requestJoin = $request->join(
-                    $this->query($data, $join),
-                    function ($baseSource) use ($join) {
-                        return $baseSource[$join['foreign']];
-                    },
-                    function ($joinedSource) use ($join) {
-                        return $joinedSource[$join['local']];
-                    },
-                    function ($baseSource, $joinedSource) use ($index, $join) {
-                        return array_merge(
-                            ArrayUtility::prefixKeys($baseSource, $index . '.'),
-                            ArrayUtility::prefixKeys($joinedSource, $join['source'] . '.')
-                        );
-                    },
-                    function ($baseSource) use ($join) {
-                        return sha1(serialize(array_values($baseSource)));
-                    }
-                );
-
-                // Add join values to list
-                $joinValues[] = $requestJoin->toArrayDeep();
+            // Handle simple type argument
+            if (!is_array($parameters)) {
+                $parameters = [$parameters];
             }
 
-            // Merge all the values gathered by JOIN constraints
-            $joinValues = array_reduce($joinValues, function ($target, $array) {
-                return ArrayUtility::merge($target, $array);
-            }, []);
+            // Handle joinned queries
+            foreach ($parameters as &$param) {
+                if (array_key_exists('from', $param)) {
+                    $param = $this->executeQuery($data, $param);
+                }
+            }
 
-            // Add join values to request
-            $request = \from(array_values($joinValues));
+            // Call selected option
+            $source = call_user_func_array([$source, $option], $parameters);
         }
 
-        return $request;
+        // Return source as array
+        return $source->toArrayDeep();
     }
 }
