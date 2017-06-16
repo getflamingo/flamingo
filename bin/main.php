6468ba89ce244a03f8c4ec50b3ee8369c2f4c5db
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2017-06-16 17:54:38 +0200

    [FEATURE] Command: Register CLI command using Commando Now supports custom configuration file using the "-f" option!

--- bin/main.php
+++ bin/main.php
@@ -2,46 +2,58 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Flamingo\Flamingo;
-use Flamingo\ErrorHandler;
 use Analog\Analog;
+use Commando\Command;
+use Flamingo\ErrorHandler;
+use Flamingo\Flamingo;
+
+// Create command controller and register options
+$command = new Command;
+
+$command
+    ->option('v')
+    ->aka('version')
+    ->describedAs('Output version information and exit.')
+    ->boolean();
+
+$command
+    ->option('f')
+    ->aka('file')
+    ->describedAs('Use a custom configuration file.');
+
+$command
+    ->option()
+    ->describedAs('Name of the task to execute.');
 
 // Create base task runner
-$flamingo = new Flamingo(
-    file_get_contents(__DIR__ . '/default.yml')
-);
+$flamingo = new Flamingo(file_get_contents(__DIR__ . '/default.yml'));
 
-// Get app configuration
-$appConf = $GLOBALS['FLAMINGO']['CONF']['App'];
-
-// No configuration file found
-if (!file_exists(getcwd() . '/flamingo.yml')) {
-    echo 'No flamingo.yml file found in ' . getcwd() . PHP_EOL;
-    exit;
-}
-
-// Add project configuration
-$flamingo->addConfiguration(
-    file_get_contents(getcwd() . '/flamingo.yml')
-);
-
-// Get log configuration
-$logConf = $GLOBALS['FLAMINGO']['CONF']['Log'];
-
-// Register error handler
-Analog::handler(ErrorHandler::init($logConf['Debug']));
-
-// Output app usage
-if (in_array('-h', $argv) || in_array('--help', $argv)) {
-    echo $appConf['Usage'];
-    exit;
-}
-
-// Output current version
-if (in_array('-v', $argv) || in_array('--version', $argv)) {
+// Output executable version
+if ($command['version']) {
+    $appConf = $GLOBALS['FLAMINGO']['CONF']['App'];
     echo $appConf['Name'] . ' ' . $appConf['Version'] . PHP_EOL;
     exit;
 }
 
+// Get configuration file from option or current dir
+$configurationFile = $command['file'] ?: getcwd() . '/flamingo.yml';
+
+// No configuration file found
+if (!file_exists($configurationFile)) {
+
+    echo $command['file']
+        ? sprintf('The configuration file "%s" does not exist!', $command['file'])
+        : sprintf('No flamingo.yml file found in %s', getcwd());
+
+    echo PHP_EOL;
+    exit;
+}
+
+// Add project configuration
+$flamingo->addConfiguration(file_get_contents($configurationFile));
+
+// Register error handler
+Analog::handler(ErrorHandler::init($GLOBALS['FLAMINGO']['CONF']['Log']['Debug']));
+
 // Run defined task
-$flamingo->run(!empty($argv[1]) ? $argv[1] : 'default');
+$flamingo->run($command[0] ?: 'default');
