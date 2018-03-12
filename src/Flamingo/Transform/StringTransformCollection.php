ae01954c0fd9fa3ee8f9410eddce3db9cb515634
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2018-03-12 17:53:59 +0100

    [FEATURE] Add a lorem ipsum random text generator

--- src/Flamingo/Transform/StringTransformCollection.php
+++ src/Flamingo/Transform/StringTransformCollection.php
@@ -3,6 +3,7 @@
 namespace Flamingo\Transform;
 
 use Flamingo\Core\TransformRuntime;
+use joshtronic\LoremIpsum;
 
 /**
  * Class StringTransformCollection
@@ -17,6 +18,40 @@ class StringTransformCollection extends AbstractTransformCollection
     {
         $arguments = $runtime->getArguments();
         $value = str_replace($arguments[0], $arguments[1], $runtime->getValue());
+        $runtime->setValue($value);
+    }
+
+    /**
+     * @param TransformRuntime $runtime
+     */
+    public function lipsum(TransformRuntime $runtime)
+    {
+        $arguments = $runtime->getArguments();
+        $generator = new LoremIpsum();
+
+        if (is_array($arguments)) {
+            $count = $arguments[0] ?: 1;
+            $type = $arguments[1] ?: '';
+        } else {
+            $count = $arguments ?: 1;
+            $type = '';
+        }
+
+        switch ($type) {
+            case 'paragraph':
+            case 'paragraphs':
+                $value = $generator->paragraphs($count);
+                break;
+            case 'sentence':
+            case 'sentences':
+                $value = $generator->sentences($count);
+                break;
+            case 'word':
+            case 'words':
+            default:
+                $value = $generator->words($count);
+        }
+
         $runtime->setValue($value);
     }
 }
